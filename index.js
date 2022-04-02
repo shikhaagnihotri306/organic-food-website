6143b21df1ff40c12165b8aaa194d0247807549d
shikhaagnihotri306 <102754174+shikhaagnihotri306@users.noreply.github.com>
2022-04-02 16:43:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+alert("danger!!!");
+alert("warning!!!");
+var message="user cancelled the prompt";
+alert(message);
+
+var message ="hello";
+var name ="shikha";
+alert(message+"there"+ name);
+
+
+var name ="shikha kumari";
+alert(name.length);
+
+var tweet=prompt("compose message");
+var tweetcount=tweet.length;
+alert("you have written"+tweetcount+"characters and you have left"+(250-tweetcount)+"characters");
+
+var a=10;
+var b=20;
+if(a==10)
+{
+  alert("sahi hai");
+}
+else
+{
+  alert("galat hai");
+}
+
+prompt("what is your name");
+prompt("what is their name");
+var lovescore=math.random()*100;
+lovescore=math.floor(lovescore)+1;
+alert("your love with that special one is"+lovescore+"%");
+if(lovescore>=60)
+{
+alert("Truelove!!!");
+}
+else
+{
+alert("change your setting");
+}
+
+var guest=["a","b","c","d"];
+alert(guest[3]);
